c214e7d2126632111c7e06727cff8abffb46f39d
Namtran25 <namtp@regit-technology.com>
2024-02-01 17:04:31 +0700

    Add 404

--- wp-content/themes/shibaura/404.php
+++ wp-content/themes/shibaura/404.php
@@ -11,49 +11,15 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shibaura' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'shibaura' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'shibaura' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$shibaura_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'shibaura' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$shibaura_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
+			<div class="container-c">
+				<header class="page-header">
+					<h1 class="page-title">404</h1>
+					<p class="text"><?php esc_html_e( 'SORRY! PAGE YOU ARE LOOKING CAN’T BE FOUND.', 'shibaura' ); ?></p>
+					<div class="desc">Go back to the <a href="<?php echo esc_url(home_url('/')); ?>">homepage</a></div>
+				</header><!-- .page-header -->
+			</div>
 		</section><!-- .error-404 -->
-
 	</main><!-- #main -->
 
 <?php
